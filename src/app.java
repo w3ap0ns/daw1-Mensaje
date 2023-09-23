8f773b07b5a681d00638856517948705d3299f82
Rafael Armas <rarmas252@live.com>
2023-09-24 00:24:01 +0200

    Daw1 - Mod1 - Mensaje

---
+++ src/app.java
@@ -0,0 +1,13 @@
+public class app {
+    public static void main(String[] arg){
+
+       /* Escribe un programa en Java (Mensaje) que dé los “buenos días”*/
+
+        /***
+         * Programa que muestra por consola "buenos días"
+         */
+        System.out.println("buenos días");
+
+
+    } // public static void main(String[] arg)
+} // public class app
